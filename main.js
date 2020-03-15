fa4963a27da2d1e66cc24ecc1a36bc9867041812
ErikaN-ir <erika.nethaway@gmail.com>
2020-03-15 23:39:07 +0000

    Taking the data outside the main function

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 var xhr = new XMLHttpRequest();
 /*JS in-built object to consume APIs*/
+var data;
+
 xhr.open("GET", "https://swapi.co/api/");
 /*GET - when retrieving data from a server*/
 /*POST - when sending data to a server*/
 xhr.send();
 /*send - sends the request*/
 
+function setData(jsonData) {
+    data = jsonData;
+    console.log(data);
+};
+
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) { 
         /*xhr object has various states while being performed
         4 means it's been completed
         status of 2 - means content delivered*/
-        document.getElementById("data").innerHTML = this.responseText;
+
+        setData(JSON.parse(this.responseText));
+
+        /*document.getElementById("data").innerHTML = this.responseText;*/
         /*the responseText that comesback from the xhr object*/
-        console.log(JSON.parse(this.responseText));
+        /*console.log(JSON.parse(this.responseText));*/
         /*response text is a string unless parsed*/
     }
 };
