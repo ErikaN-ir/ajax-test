b9f6b4e59d86835c68c02ca185846214efebab24
ErikaN-ir <erika.nethaway@gmail.com>
2020-03-16 16:18:34 +0000

    trying callback function

--- main.js
+++ main.js
@@ -1,35 +1,17 @@
-var xhr = new XMLHttpRequest();
+function getData(cb) { /*cb is the argument of getData*/
+    var xhr = new XMLHttpRequest();
 /*JS in-built object to consume APIs*/
-var data;
 
-xhr.open("GET", "https://swapi.co/api/");
-/*GET - when retrieving data from a server*/
-/*POST - when sending data to a server*/
-xhr.send();
-/*send - sends the request*/
+    xhr.open("GET", "https://swapi.co/api/");
+    xhr.send();
 
-/*function setData(jsonData) {
-    data = jsonData;
-}*/
-
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) { 
-        /*xhr object has various states while being performed
-        4 means it's been completed
-        status of 2 - means content delivered*/
-
-        data = JSON.parse(this.responseText);
-
-        /*setData(JSON.parse(this.responseText));*/
-
-        /*document.getElementById("data").innerHTML = this.responseText;*/
-        /*the responseText that comesback from the xhr object*/
-        /*console.log(JSON.parse(this.responseText));*/
-        /*response text is a string unless parsed*/
-    }
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) { 
+            cb(JSON.parse(this.responseText)); /*giving cb an arguement*/
+        }
+    };
 };
 
-setTimeout(function() {
+getData(function(data) { /*data is the name of the arguement of cb*/
     console.log(data);
-}, 1000);
-/* 1000 - time in milliseconds that we want our programme to wait for*/
+});
